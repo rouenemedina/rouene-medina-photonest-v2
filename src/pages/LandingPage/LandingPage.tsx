45acaea72f8ee4451a998b49fd1aef065e5c40ca
rouenemedina <rouenemedina@gmail.com>
2024-08-08 23:23:16 -0400

    created the landing page content and styling

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,27 @@
 import "./LandingPage.scss";
-// import React from 'react';
+import { Link } from "react-router-dom";
+import React from "react";
 
-function LandingPage() {
-    return (
-        <>
-            
-        </>
-    );
-}
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <main className="landing">
+        <h1 className="landing__title">Discover your next journey</h1>
+        <section className="landing__container">
+          <article className="landing__subcontainer">
+            <Link to="" className="landing__link">
+              <h2 className="landing__description">Photographer</h2>
+            </Link>
+          </article>
+          <article className="landing__subcontainer">
+            <Link to="" className="landing__link">
+              <h2 className="landing__description">Client</h2>
+            </Link>
+          </article>
+        </section>
+      </main>
+    </>
+  );
+};
 
 export default LandingPage;
