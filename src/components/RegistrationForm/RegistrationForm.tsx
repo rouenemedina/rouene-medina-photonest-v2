b00d85f66eae82f93aad4045aa3444ddbcae0038
rouenemedina <rouenemedina@gmail.com>
2024-08-09 12:03:05 -0400

    created a registration form component with boilerplate code

---
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,18 @@
+import "./RegistrationForm.scss";
+import React from 'react';
+
+const RegistrationForm: React.FC = () => {
+    return (
+        <>
+        <form className="form">
+            <section>
+                <article>
+                    
+                </article>
+            </section>
+        </form>
+        </>
+    );
+};
+
+export default RegistrationForm;
--- src/pages/SignUpPage/SignUpPage.tsx
+++ src/pages/SignUpPage/SignUpPage.tsx
@@ -4,23 +4,25 @@ import React from "react";
 
 const SignUpPage: React.FC = () => {
   return (
-    <main className="registration">
-      <section className="registration__logo"></section>
-      <section className="registration__container">
-        <article className="registration__subcontainer">
-          <div className="registration__card">
-            <h1 className="registration__title">CREATE NEW ACCOUNT</h1>
-            <div className="registration__subcard">
-              <h3 className="registration__subtitle">Already a member? </h3>
-              <Link to="" className="registration__link">
-                <h3 className="registration__subtitle">Log In</h3>
-              </Link>
+    <>
+      <main className="registration">
+        <section className="registration__logo"></section>
+        <section className="registration__container">
+          <article className="registration__subcontainer">
+            <div className="registration__card">
+              <h1 className="registration__title">CREATE NEW ACCOUNT</h1>
+              <div className="registration__subcard">
+                <h3 className="registration__subtitle">Already a member? </h3>
+                <Link to="" className="registration__link">
+                  <h3 className="registration__subtitle">Log In</h3>
+                </Link>
+              </div>
             </div>
-          </div>
-        </article>
-        <article className="registration__form"></article>
-      </section>
-    </main>
+          </article>
+          <article className="registration__form"></article>
+        </section>
+      </main>
+    </>
   );
 };
 
