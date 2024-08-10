084681d02c519da4aa4635dbcb310f96cced6b10
rouenemedina <rouenemedina@gmail.com>
2024-08-09 20:05:09 -0400

    created a login page with boilerplate

---
+++ src/pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,12 @@
+import "./LoginPage.scss";
+import React from 'react';
+
+const LoginPage: React.FC = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default LoginPage;
