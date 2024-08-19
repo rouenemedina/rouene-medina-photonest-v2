9f38cc4d013655003cc3ce67a700a56c2afc27c7
rouenemedina <rouenemedina@gmail.com>
2024-08-19 18:57:10 -0400

    created a Navigation component with boilerplate

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,12 @@
+import "./Navigation.scss";
+import React from 'react';
+
+const Navigation: React.FC = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Navigation;
