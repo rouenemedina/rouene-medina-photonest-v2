1992c4ea24d379f45956c994b7a32c0e344aa888
rouenemedina <rouenemedina@gmail.com>
2024-08-19 17:44:42 -0400

    added the header and footer to the contact page, added classNames for styling

--- src/pages/ContactPage/ContactPage.tsx
+++ src/pages/ContactPage/ContactPage.tsx
@@ -1,13 +1,30 @@
 import "./ContactPage.scss";
-import React from 'react';
+import React from "react";
 import ContactForm from "../../components/FormContact/FormContact";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 const ContactPage: React.FC = () => {
-    return (
-        <>
-          <ContactForm fields={["name", "email", "message"]} successMessage={""}/>  
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <main className="contact">
+        <section className="contact__section">
+          <article className="contact__card">
+            <h1 className="contact__title">Contact Us</h1>
+          </article>
+          <ContactForm
+            fields={["name", "email", "message"]}
+            successMessage={""}
+          />
+        </section>
+        <section className="contact__feature">
+            <img></img>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default ContactPage;
