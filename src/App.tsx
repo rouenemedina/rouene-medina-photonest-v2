86f5d5cbe9bff443cc306a66345194932631ca29
rouenemedina <rouenemedina@gmail.com>
2024-08-20 17:40:43 -0400

    created a Template component with boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import ContactPage from "./pages/ContactPage/ContactPage";
+import PortfolioPage from "./pages/PortfolioPage/PortfolioPage";
 
-function App() {
+const App: React.FC = () => {
   return (
     <>
       <BrowserRouter>
@@ -14,6 +16,7 @@ function App() {
           <Route path="/register" element={<SignUpPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
           <Route path="/dashboard" element={<DashboardPage />}></Route>
+          <Route path="/portfolio" element={<PortfolioPage />}></Route>
           <Route path="/contactus" element={<ContactPage />}></Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/Template/Template.tsx
@@ -0,0 +1,12 @@
+import "./Template.scss";
+import React from 'react';
+
+const Template: React.FC = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default Template;
